b856f710bee31f501378bc226f378e80a8ef54a6
Matthieu Gavaudan <mlg2199@columbia.edu>
2015-10-19 18:30:18 -0400

    got most functionalities working - background is white

--- js/content.js
+++ js/content.js
@@ -1,27 +1,46 @@
-     // Instantiation
-            var theater = new TheaterJS();
+var theater = new TheaterJS();
 
-            // Describe actors
-            theater
-                .describe("Mat", .8, "#mat")
+theater
+    .describe("Mat", 1, "#mat")
 
-            // Write the scenario
-            theater
-                .write("Mat:Hey.", 400, " I'm Matthieu.", 600)
-                .write("Mat:Welcome to my website.", 400) 
-                .write("Mat:Check out my projects.", 400)
-                .write("Mat:I hope you like them.", 800)
-                .write("Mat:Thanks for stopping by.", 600)
-                .write("Mat:Enjoy!")
+theater
+    .write("Mat:Hey.", 100, " I'm Matthieu.", 600)
+    .write("Mat:Welcome to my website.", 800) 
+    .write("Mat:Thanks for stopping by.", 800)
+    .write("Mat:Enjoy!")
 
-            // Listen to theater's events
-            theater
-                .on("say:start, erase:start", function () {
-                    // add blinking caret
-                })
-                .on("say:end, erase:end", function () {
-                    // remove blinking caret
-                })
-                .on("*", function () {
-                    // do something
-                }); 
+theater
+    .on("say:start, erase:start", function () {
+    })
+    .on("say:end, erase:end", function () {
+    })
+
+
+
+$( "#aboutme" ).click(function() {
+   $(".navbar").css("display", "inline-block");
+   $(".title").fadeOut( "fast", function() {});
+   $(".scene").fadeOut( "fast", function() {});
+   $(".links").fadeOut( "fast", function() {});
+   $( ".wrapper" ).animate({
+    height: "+=40em",
+    top: "-=30%"
+  }, "slow" );
+   $(".wrapper" ).css( "background", "white" );
+   $(".navbar").fadeIn("fast", function(){});
+});
+
+
+$( "#projects" ).click(function() {
+   $(".navbar").css("display", "inline-block");
+   $(".title").fadeOut( "fast", function() {});
+   $(".scene").fadeOut( "fast", function() {});
+   $(".links").fadeOut( "fast", function() {});
+   $( ".wrapper" ).animate({
+    height: "+=40em",
+    top: "-=30%"
+  }, "slow" );
+   $(".wrapper" ).css( "background", "white" );
+   $(".navbar").fadeIn("fast", function(){});
+});
+
