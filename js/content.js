6d12f04c14629872c121c0dc2950e893d8e8c57f
Matthieu Gavaudan <mlg2199@columbia.edu>
2015-10-19 23:47:02 -0400

    raw ver finished

--- js/content.js
+++ js/content.js
@@ -23,11 +23,13 @@ $( "#aboutme" ).click(function() {
    $(".scene").fadeOut( "fast", function() {});
    $(".links").fadeOut( "fast", function() {});
    $( ".wrapper" ).animate({
-    height: "+=40em",
+    height: "+=35em",
     top: "-=30%"
   }, "slow" );
    $(".wrapper" ).css( "background", "white" );
    $(".navbar").fadeIn("fast", function(){});
+   $(".wrapperNav").css("background", "rgba(0, 0, 0, 0.5)");
+   $(".aboutmepage").css("display", "block");
 });
 
 
@@ -37,10 +39,25 @@ $( "#projects" ).click(function() {
    $(".scene").fadeOut( "fast", function() {});
    $(".links").fadeOut( "fast", function() {});
    $( ".wrapper" ).animate({
-    height: "+=40em",
+    height: "+=35em",
     top: "-=30%"
   }, "slow" );
    $(".wrapper" ).css( "background", "white" );
    $(".navbar").fadeIn("fast", function(){});
+   $(".wrapperNav").css("background", "rgba(0, 0, 0, 0.5)");
+   $(".projectpage").css("display", "block");
 });
+
+$( "#pro" ).click(function() {
+   $(".aboutmepage").css("display", "none");
+   $(".projectpage").css("display", "block");
+});
+
+$( "#abo" ).click(function() {
+   $(".projectpage").css("display", "none");
+   $(".aboutmepage").css("display", "block");
+});
+
+
+
 
