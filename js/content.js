d2d26516198558673ef16b246c798df4cf461ac9
Matthieu Gavaudan <mlg2199@columbia.edu>
2015-10-25 03:58:12 -0400

    changed entire website

--- js/content.js
+++ js/content.js
@@ -1,22 +1,3 @@
-var theater = new TheaterJS();
-
-theater
-    .describe("Mat", 1, "#mat")
-
-theater
-    .write("Mat:Hey.", 100, " I'm Matthieu.", 600)
-    .write("Mat:Welcome to my website.", 800) 
-    .write("Mat:Thanks for stopping by.", 800)
-    .write("Mat:Enjoy!")
-
-theater
-    .on("say:start, erase:start", function () {
-    })
-    .on("say:end, erase:end", function () {
-    })
-
-
-
 $( "#aboutme" ).click(function() {
    $(".navbar").css("display", "inline-block");
    $(".title").fadeOut( "fast", function() {});
